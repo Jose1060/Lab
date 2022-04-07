45515bf7ff7ae525fda49e45696ffe380acd60ba
Jose1060 <josean-1060@hotmail.com>
2022-04-07 18:44:44 -0500

    Primer Commit

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { render } from "react-native/Libraries/Renderer/implementations/ReactNativeRenderer-prod";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			textValue: "",
+			count: 0,
+		};
+	}
+	changeTextInput = (text) => {
+		this.setState({
+			textValue: text,
+		});
+	};
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.text}>
+					<Text>Ingrese edad</Text>
+				</View>
+
+				<TextInput
+					style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+					onChange={this.changeTextInput(text)}
+					value={this.state.textValue}
+				/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
